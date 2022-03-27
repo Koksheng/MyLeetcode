62fd6145a3c21a8ff87fb0723dc90c9b0f60ac9a
Koksheng <koksheng0406@gmail.com>
2022-03-27 22:03:12 +0800

    align code

--- 9. Palindrome Number/Program.cs
+++ 9. Palindrome Number/Program.cs
@@ -1,15 +1,15 @@
 public static bool Palindrome(int x)
-        {
-            int temp = 0;
-            int n = x;
-            while (n > 0)
-            {
-                temp = temp * 10 + n % 10;
-                n = n / 10;
-            }
-            if (temp == x)
-            {
-                return true;
-            }
-            return false;
-        }
+{
+    int temp = 0;
+    int n = x;
+    while (n > 0)
+    {
+        temp = temp * 10 + n % 10;
+        n = n / 10;
+    }
+    if (temp == x)
+    {
+        return true;
+    }
+    return false;
+}
